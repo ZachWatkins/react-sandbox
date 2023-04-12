528146ebec054c554817220490fda50473eb9a83
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-04-12 06:40:03 -0500

    Add offline caching to the SessionContext component

--- src/components/SessionContext.jsx
+++ src/components/SessionContext.jsx
@@ -1,38 +1,65 @@
 import React from 'react'
 
-const DEFAULT_SESSION = { color: 'blue', items: [] }
-
-const SessionContext = React.createContext()
-const A = false
-
+const DEFAULT_SESSION = { collections: {} }
+const parseStoredObject = function(key, initial = '{}') {
+  try {
+    return JSON.parse(localStorage.getItem(key) || initial)
+  } catch (error) {
+    return JSON.parse(initial)
+  }
+}
 const sessionClient = {
-  updateSession(session, updates) {
+  updateSession: function(session, updates) {
     return Promise.resolve({ ...session, ...updates })
   },
 }
 
 /**
+ * Create a new Session state object.
  *
- * @param state
- * @param action
+ * @param {object} [props] - The session properties as JSON or an object.
+ */
+function Session(props) {
+  if (props === undefined) {
+    props = parseStoredObject(this.name)
+  }
+  Object.assign(this, props)
+  localStorage.setItem(this.name, JSON.stringify(this))
+}
+Session.prototype = DEFAULT_SESSION
+
+const SessionContext = React.createContext()
+
+/**
+ * Reducer function for modifying the session state.
+ *
+ * @param {object} state - The current session state.
+ * @param {object} action - The action to take when modifying the state.
+ * @param {string} action.type - The type of action to take.
+ * @param {object} action.payload - The payload of the action.
+ * @returns {object} The new session state.
  */
 function sessionReducer(state, action) {
   switch (action.type) {
   case 'update':
-    return { ...state, ...action.payload }
+    return new Session(Object.assign(state, action.payload))
+  case 'reset':
+    return new Session(DEFAULT_SESSION)
   default:
     throw new Error(`Unhandled action type: ${action.type}`)
   }
 }
 
 /**
- * @param root0
- * @param root0.children
- * @example
+ * The SessionProvider component.
  *
+ * @param {object} props - The props for the component.
+ * @param {React.ReactNode} props.children - The child node(s) to render.
+ * @returns {React.ReactElement} The SessionProvider component.
+ * @example
  * ```
  * <SessionProvider>
- *     <MyComponent />
+ *   <YourComponent />
  * </SessionProvider>
  * ```
  */
@@ -45,11 +72,14 @@ function SessionProvider({ children }) {
 }
 
 /**
- * @example
+ * A hook for consuming the session context.
  *
+ * @returns {object|undefined} The session context.
+ * @throws {Error} Will throw an error if the hook is used outside of a SessionProvider.
+ * @example
  * ```
  * function YourComponent() {
- *     const {session, dispatchSession} = useSession();
+ *   const {session, dispatchSession} = useSession();
  * }
  * ```
  */
@@ -62,18 +92,20 @@ function useSession() {
 }
 
 /**
- * @param dispatch
- * @param session
- * @param updates
+ * A hook for updating the session asynchronously.
+ *
+ * @param {Function} dispatch - The dispatch function from the useSession hook.
+ * @param {object} session - The current session state.
+ * @param {any} updates - The updates to apply to the session.
  * @example `const [{session, status, error}, sessionDispatch] = useSession();`
  */
 async function updateSession(dispatch, session, updates) {
-  dispatch({ type: 'start update', updates })
+  dispatch({ type: 'update:start', payload: updates })
   try {
     const updatedSession = await sessionClient.updateSession(session, updates)
-    dispatch({ type: 'finish update', updatedSession })
+    dispatch({ type: 'update:finish', payload: updatedSession })
   } catch (error) {
-    dispatch({ type: 'fail update', error })
+    dispatch({ type: 'update:error', payload: error })
   }
 }
 
